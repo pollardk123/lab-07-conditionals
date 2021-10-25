916a1a71d5457e1d8af3c50b6475cae8488e3184
pollardk123 <pollardk836@gmail.com>
2021-10-25 17:52:44 -0400

    corrected the following files before uploading

---
+++ cake.py
@@ -0,0 +1,7 @@
+cake = input("Do you want cake? (yes or no)")
+if cake == "yes" or cake == "Yes" or cake == "y":
+    print("Have some cake.");
+elif cake == "no" or cake == "No" or cake == "n":
+    print("No cake for you!!!!");
+else:
+    print("ERROR: Sorry,I don't understand!");
